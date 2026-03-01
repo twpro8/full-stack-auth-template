b407a36b2f6860e12fd1ac7203073c54089bbfea
twpro8 <twpro8@gmail.com>
2026-03-01 17:06:56 +0300

    chore: initialize alembic and run migrations

---
+++ backend/src/hydra/migrations/versions/2026_03_01_1701-44eddd3c7a10_create_users_table.py
@@ -0,0 +1,46 @@
+"""create users table
+
+Revision ID: 44eddd3c7a10
+Revises:
+Create Date: 2026-03-01 17:01:42.143638
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision: str = "44eddd3c7a10"
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("username", sa.String(length=128), nullable=False),
+        sa.Column("email", sa.String(length=128), nullable=False),
+        sa.Column("password_hash", sa.String(length=128), nullable=False),
+        sa.Column("full_name", sa.String(length=128), nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False),
+        sa.Column("is_superuser", sa.Boolean(), nullable=False),
+        sa.Column(
+            "created_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("email"),
+        sa.UniqueConstraint("username"),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("users")
