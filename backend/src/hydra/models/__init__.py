42f212ec1db83ad5066473aaba672b2db40f7b01
twpro8 <twpro8@gmail.com>
2026-03-01 17:04:18 +0300

    feat: add user sqlalchemy orm model

---
+++ backend/src/hydra/models/__init__.py
@@ -0,0 +1,5 @@
+__all__ = [
+    "UserOrm",
+]
+
+from .user import UserOrm
---
+++ backend/src/hydra/models/user.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, String, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from hydra.database import Base
+
+
+class UserOrm(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(128), unique=True)
+    email: Mapped[str] = mapped_column(String(128), unique=True)
+    password_hash: Mapped[str] = mapped_column(String(128))
+    full_name: Mapped[str] = mapped_column(String(128))
+    is_active: Mapped[bool] = mapped_column(default=True)
+    is_superuser: Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
