89f7d461fd64f49a2b3b4fb6987b6400cceb20b5
twpro8 <twpro8@gmail.com>
2026-03-04 16:20:45 +0300

    feat: handle exceptions when creating a new user

--- backend/src/hydra/exceptions.py
+++ backend/src/hydra/exceptions.py
@@ -29,3 +29,11 @@ class UserAlreadyExistsError(ObjectAlreadyExistsError):
 
 class UserNotFoundError(ObjectNotFoundError):
     detail = "User not found."
+
+
+class UsernameAlreadyExistsError(ObjectAlreadyExistsError):
+    detail = "Username already exists."
+
+
+class EmailAlreadyExistsError(ObjectAlreadyExistsError):
+    detail = "Email already exists."
