7a4377684f215935de327aac2a17ffef9a65ec44
twpro8 <twpro8@gmail.com>
2026-03-04 16:20:45 +0300

    feat: handle exceptions when creating a new user

--- backend/src/hydra/repositories/user.py
+++ backend/src/hydra/repositories/user.py
@@ -1,7 +1,10 @@
 from typing import Sequence
 
+from asyncpg.exceptions import UniqueViolationError  # type: ignore[import-untyped]
 from sqlalchemy import insert, select, func
+from sqlalchemy.exc import IntegrityError
 
+from hydra.exceptions import UsernameAlreadyExistsError, EmailAlreadyExistsError
 from hydra.models import UserOrm
 from hydra.repositories import BaseRepository
 from hydra.schemas.user import UserCreate, User
@@ -10,7 +13,20 @@ from hydra.schemas.user import UserCreate, User
 class UserRepository(BaseRepository):
     async def create(self, data: UserCreate) -> None:
         statement = insert(UserOrm).values(data.model_dump())
-        await self.session.execute(statement)
+        try:
+            await self.session.execute(statement)
+        except IntegrityError as e:
+            cause = getattr(e.orig, "__cause__", None)
+            constraint = getattr(cause, "constraint_name", None)
+            if isinstance(cause, UniqueViolationError):
+                match constraint:
+                    case "users_username_key":
+                        raise UsernameAlreadyExistsError from e
+                    case "users_email_key":
+                        raise EmailAlreadyExistsError from e
+                    case _:
+                        raise
+            raise
 
     async def get_all(self, offset: int, limit: int) -> Sequence[User]:
         query = select(UserOrm).offset(offset).limit(limit)
