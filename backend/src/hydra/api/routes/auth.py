c58b1550fad97a779b1a0855beee2ae1a4ee87b4
John Davis <twpro8@gmail.com>
2026-03-13 02:02:43 +0300

    feat: logout endpoint

--- backend/src/hydra/api/routes/auth.py
+++ backend/src/hydra/api/routes/auth.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 from fastapi.responses import Response
 
-from hydra.api.dependencies import AuthServiceDep
+from hydra.api.dependencies import AuthServiceDep, get_current_user_id
 from hydra.schemas.auth import RegisterForm, LoginForm
+from hydra.schemas.common import SuccessResponse
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
@@ -12,7 +13,8 @@ async def authenticate(
     form_data: LoginForm,
     service: AuthServiceDep,
     response: Response,
-) -> dict[str, str]:
+) -> SuccessResponse:
+    # TODO: Create refresh token as well
     access_token = await service.login(
         email=form_data.email,
         password=form_data.password,
@@ -24,17 +26,37 @@ async def authenticate(
         secure=True,
         samesite="strict",
     )
-    return {"status": "OK"}
+    return SuccessResponse()
 
 
 @router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register(
     form_data: RegisterForm,
     service: AuthServiceDep,
-) -> dict[str, str]:
+) -> SuccessResponse:
     await service.register(
         full_name=form_data.full_name,
         email=form_data.email,
         password=form_data.password,
     )
-    return {"status": "OK"}
+    return SuccessResponse()
+
+
+# TODO: Refresh access token
+
+
+@router.post(
+    "/logout",
+    status_code=status.HTTP_204_NO_CONTENT,
+    dependencies=[
+        Depends(get_current_user_id)
+    ],  # check if there is a valid access token and user logged in
+)
+async def logout(response: Response) -> None:
+    # TODO: Invalidate (revoke) refresh token
+    response.delete_cookie(
+        key="access_token",
+        httponly=True,
+        secure=True,
+        samesite="strict",
+    )
