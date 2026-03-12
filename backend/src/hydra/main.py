cbcb77b135fb3c34139a06129fa7e7179e05dc86
John Davis <twpro8@gmail.com>
2026-03-12 03:12:42 +0300

    feat: add cors middleware

--- backend/src/hydra/main.py
+++ backend/src/hydra/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from hydra.config import settings
 from hydra.api import master_router
 from hydra.errors import HydraError, app_exception_handler
 
 app = FastAPI(title=settings.PROJECT_NAME)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALL_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.add_exception_handler(HydraError, app_exception_handler)  # type: ignore
 app.include_router(master_router, prefix=settings.API_V1_STR)
 
